9cf451ffc2d69de14a48ff5e5a4452bae7296235
Vernon Gussie <164996107+vatwhirl@users.noreply.github.com>
2024-03-26 09:11:58 +0200

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,31 @@
+  while (stacksize > 0)
+        {
+            (int i1, int t1) = stack[stacksize - 1];
+            stacksize--;
+
+            int x1 = i1 % MX;
+            int y1 = i1 / MX;
+
+            for (int d = 0; d < 4; d++)
+            {
+                int x2 = x1 + dx[d];
+                int y2 = y1 + dy[d];
+                if (!periodic && (x2 < 0 || y2 < 0 || x2 + N > MX || y2 + N > MY)) continue;
+
+                if (x2 < 0) x2 += MX;
+                else if (x2 >= MX) x2 -= MX;
+                if (y2 < 0) y2 += MY;
+                else if (y2 >= MY) y2 -= MY;
+
+                int i2 = x2 + y2 * MX;
+                int[] p = propagator[d][t1];
+                int[][] compat = compatible[i2];
+  for (int l = 0; l < p.Length; l++)
+                {
+                    int t2 = p[l];
+                    int[] comp = compat[t2];
+
+                    comp[d]--;
+                    if (comp[d] == 0) Ban(i2, t2);
+                }
+            }
